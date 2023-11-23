0b3eda8e2322e7ec41b7c1618361252d52b8b7d4
zeereoreo <wltjrcjf12@naver.com>
2023-11-23 15:53:42 +0900

    feat : naver map + maker

--- lib/map.dart
+++ lib/map.dart
@@ -5,21 +5,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 void main() async {
-  await _initialize();
   runApp(const NaverMapApp());
 }
 
-// 지도 초기화하기
-Future<void> _initialize() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await NaverMapSdk.instance.initialize(
-      clientId: 'mO4HPigLnqb3apzd2L2F',     // 클라이언트 ID 설정
-      onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed")
-  );
-}
+
 
 class NaverMapApp extends StatelessWidget {
   const NaverMapApp({Key? key});
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -30,14 +24,45 @@ class NaverMapApp extends StatelessWidget {
       home: Scaffold(
         body: NaverMap(
           options: const NaverMapViewOptions(
-            indoorEnable: true,             // 실내 맵 사용 가능 여부 설정
-            locationButtonEnable: false,    // 위치 버튼 표시 여부 설정
-            consumeSymbolTapEvents: false,  // 심볼 탭 이벤트 소비 여부 설정
+            initialCameraPosition: NCameraPosition(
+                target: NLatLng(37.568963, 126.646476),
+                zoom: 10,
+                bearing: 0,
+                tilt: 0
+            ),
+            mapType: NMapType.basic,   // 맵 스타일
+            liteModeEnable: true,   // 경량모드
+            indoorEnable: true,             // 실내 맵 사용 가능 여부
+            locationButtonEnable: true,    // 내 위치 표시 버튼
+            consumeSymbolTapEvents: false,  // 심볼 탭 이벤트 소비 여부
+            zoomGesturesEnable: true,       // 줌 설정
+            rotationGesturesEnable: false, // 회전
+            tiltGesturesEnable: false,  // 기울기
+            minZoom: 10, // default is 0
+            maxZoom: 16, // default is 21
+            extent: NLatLngBounds(          // 한반도 내로 제한
+              southWest: NLatLng(31.43, 122.37),
+              northEast: NLatLng(44.35, 132.0),
+            ),
           ),
-          onMapReady: (controller) async {                // 지도 준비 완료 시 호출되는 콜백 함수
+          onMapReady: (controller) async {
+            // final marker1 = NMarker(id: '1', position: (37.568963,126.646476));
             mapControllerCompleter.complete(controller);  // Completer에 지도 컨트롤러 완료 신호 전송
             log("onMapReady", name: "onMapReady");
-          },
+                final marker = NMarker(
+                id: 'test',
+                position:
+                const NLatLng(37.568963, 126.646476));
+                final marker1 = NMarker(
+                id: 'test1',
+                position:
+                const NLatLng(37.606932467450326, 127.05578661133796));
+                controller.addOverlayAll({marker, marker1});
+
+                final onMarkerInfoWindow =
+                NInfoWindow.onMarker(id: marker.info.id, text: "디고 빈");
+                marker.openInfoWindow(onMarkerInfoWindow);
+                },              // 지도 준비 완료 시 호출되는 콜백 함수
         ),
       ),
     );
