3ea81c12af54ad25db5b99142cd82def975793de
zeereoreo <wltjrcjf12@naver.com>
2023-12-08 14:35:18 +0900

    feat : point page lottie 적용

--- lib/point.dart
+++ lib/point.dart
@@ -1,6 +1,7 @@
 import 'package:deego_client/earn.dart';
 import 'package:deego_client/header.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class Point extends StatelessWidget {
   const Point({super.key});
@@ -17,41 +18,59 @@ class Point extends StatelessWidget {
           const Header(),
           Container(
             // decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 1)),
-            margin: EdgeInsets.all(20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
               children:  [
                 Container(
-                    decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 1)),
-                    margin: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        Text('현재 보유 포인트'),
-                        TextButton(onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => Earn()), // Earn 클래스의 인스턴스 반환
-                          );
-                        },
-                            child: const Text('적립 내역',style: TextStyle(
-                          fontSize: 10,
-                          color: Color(0xFFDFDFDF),
-                          decoration: TextDecoration.underline,
-                        ),))
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black)
+                          ),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width/2,
+                                color: Color(0x5000BEFF),
+                                height: MediaQuery.of(context).size.height/6,
+                                margin: EdgeInsets.all(10),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(top: 10),
+                                      height: MediaQuery.of(context).size.height/18,
+                                      child: Text('현재 포인트',style: TextStyle(color: Colors.white,),),
+                                    ),
+                                    Container(
+                                      // margin: EdgeInsets.all(10),
+                                      height: MediaQuery.of(context).size.height/18,
+                                      child: Text('0 P', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width/3,
+                                height: MediaQuery.of(context).size.height/6,
+                                // decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 1)),
+                                child: Lottie.asset("assets/lottie/point_lottie.json"),
+                              )
+                            ],
+                          ),
+                        ),
 
-                      ],
-                    )),
+                      ),
+                    ],
+                  ),
+                ),
                 Container(
-                  margin: EdgeInsets.all(20),
-                  width: 100,
-                  // decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 1)),
-                  child:
-                    const Text('0', style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
 
-                ),),),
+                ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -72,23 +91,86 @@ class Point extends StatelessWidget {
                           ],
                         ),
                       ),
-                    Container()
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black)
+                      ),
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height/10,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width/3,
+                            height: MediaQuery.of(context).size.height/15,
+                            child: TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                backgroundColor: Colors.grey,
+                                padding: EdgeInsets.all(16),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  '적립 내역',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Color(0xFFCCCCCC),
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width/3,
+                            height: MediaQuery.of(context).size.height/15,
+                            child: TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                backgroundColor: Colors.grey,
+                                padding: EdgeInsets.all(16),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  '구매 내역',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Color(0xFFCCCCCC),
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                     ],
                 )
               ],
             ),
           ),
           Container(
-            height: 100,
+            height: MediaQuery.of(context).size.height/10,
             child: Row(
               children: [
                 Container(
                   decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 1)),
-                  height: 150, width: 200,
+                    height: MediaQuery.of(context).size.height/10,
+                    width: MediaQuery.of(context).size.width/3,
                     child : (
-                        Text('사진 들어갈 자리'))),
-                        Text('시흥 화폐 시루'),
-                        Text('1만원권'),
+                        Text('사진 들어갈 자리'))
+                ),
+                Container(
+                    decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 1)),
+                    height: MediaQuery.of(context).size.height/10,
+                    width: MediaQuery.of(context).size.width/3,
+                    alignment: Alignment.center,
+                    child : (
+                        Text('네이버 포인트로 환전하기'))
+                ),
               ],
             ),
             // decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 1)),
